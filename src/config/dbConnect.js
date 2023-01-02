8b6902309395ca74d6a990bdf5f74aae81150a4c
AnthoniSena <115597876+AnthoniSena@users.noreply.github.com>
2023-01-02 16:02:51 -0300

    Corrigindo String de conexão

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect("mongodb+srv://anthoni:12345@anthoni.opu3fpu.mongodb.net/Node"); // utilizar string de conexão disponivel no site do banco de dados
+mongoose.connect("mongodb+srv://<user>:<password>@anthoni.opu3fpu.mongodb.net/Node"); // utilizar string de conexão disponivel no site do banco de dados
 
 let db = mongoose.connection;
 
